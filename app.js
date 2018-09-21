432b707718ea0717de87d39eaf8822734dc267ba
Tanzimul Hasib <tanzimul.hasib@gmail.com>
2018-09-21 16:00:33 +0600

    all new files created & added with boostrap

--- app.js
+++ app.js
@@ -1,47 +1,50 @@
-var express= require('express');
-var path=require('path');
+var express = require('express');
+var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var expHandlebars =require('express-handlebars');
-var expValodator =require('express-validator');
-var flash=require('connect-flash');
+var exphbs = require('express-handlebars');
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
 var session = require('express-session');
-var passport= require('passport');
-var localStrategy= require('pasport-local'),Strategy;
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 var mongo = require('mongodb');
-var mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/loginapp');
-var db =mongoose.connections;
+var mongoose = require('mongoose');
 
-var routes=require('./routes/index')
-var users=require('./routes/users')
-// Initialize app
-var app= express();
+mongoose.connect('mongodb://localhost/loginapp', { useNewUrlParser: true });
+var db = mongoose.connection;
 
-//view Engine setup
-app.set('views', path.join(__dirname,'views'));
-app.engine('handlebars',expHandlebars({defaultLayout:'layout'}));
-app.set('view engine','handlebars');
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
-//Middlewere setup
-app.use(bodyParser.urlencoded({ extended: false }));
+// Init App
+var app = express();
+
+// View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs({defaultLayout:'layout'}));
+app.set('view engine', 'handlebars');
+
+// BodyParser Middleware
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-//Set public folder
-app.use(express.static(path.join(__dirname,'public')));
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
 
-//Express session
+// Express Session
 app.use(session({
-    secret:'secret',
-    saveUninitialized:true,
+    secret: 'secret',
+    saveUninitialized: true,
     resave: true
 }));
 
-//Passport Init
+// Passport init
 app.use(passport.initialize());
 app.use(passport.session());
-// Express validator #catvan *Middlewere
+
+// Express Validator
 app.use(expressValidator({
     errorFormatter: function(param, msg, value) {
         var namespace = param.split('.')
@@ -56,39 +59,29 @@ app.use(expressValidator({
             msg   : msg,
             value : value
         };
-    };
+    }
 }));
 
-//Connect-Flash Middlewere
-
+// Connect Flash
 app.use(flash());
 
-//Global varibles
-app.use(function (req,res,next) {
-    res.local.success_msg= req.flash('success_msg');
-    res.local.erorr_msg= req.flash('erorr_msg');
-    res.local.erorr= req.flash('erorr');
+// Global Vars
+app.use(function (req, res, next) {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    res.locals.user = req.user || null;
     next();
-})
-
-app.use('/' , routes);
-app.use('/users', users);
-
-
-app.set('port',(process.env.PORT || 3000 ));
-app.listen(app.get(port),function () {
-  console.log('Server started at port'+ app.get(port));
 });
 
 
 
+app.use('/', routes);
+app.use('/users', users);
 
+// Set Port
+app.set('port', (process.env.PORT || 3000));
 
-
-
-
-
-
-
-
-
+app.listen(app.get('port'), function(){
+    console.log('Server started on port '+app.get('port'));
+});
